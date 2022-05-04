8286bf773bd37816deb98d7f161e3b7e0b79f5df
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-04 23:48:32 +0000

    fix main.cs

--- main.cs
+++ main.cs
@@ -12,14 +12,20 @@ class Program
         // This program calculates the volume of a sphere
         double radiusOfTheSphere;
         double volume;
+      
         Console.WriteLine("This program calculates the volume of a sphere.");
         Console.WriteLine("");
-
+      
+        // input
         Console.WriteLine("Enter the radius.");
         radiusOfTheSphere = (Convert.ToInt32(Console.ReadLine()));
         Console.WriteLine("");
+      
+        // process
         volume = (double)4 / (double)3 * Math.PI * (radiusOfTheSphere * radiusOfTheSphere * radiusOfTheSphere);
         Console.WriteLine("");
+
+        // output
         Console.Write("volume of the sphere = " + volume.ToString("0.00") + "cm³");
 
         Console.WriteLine("\nDone.");
